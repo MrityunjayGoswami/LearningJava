ce2a49298959ce5fd2a8695f5077067a371e9b70
imvivek71 <vivekgoswami71@gmail.com>
2019-02-13 15:52:08 +0530

    b

---
+++ src/learningjava/AddTwoNumbers.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learningjava;
+
+/**
+ *
+ * @author vivek
+ */
+public class AddTwoNumbers {
+    
+    public static void main(String[] args) {
+       int x =1;
+       int y = 2;
+      int sum = x+y;
+        System.out.println(sum);
+    }
+}
